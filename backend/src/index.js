2f44892617ccbcf337038c30743de84158e569d1
Gabriel Machado <g_machado1@hotmail.com>
2018-11-06 14:16:17 -0200

    Feito rotas e controller de tweets, e adicionado mongoose e nodemon

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://goweek:weekgo123@ds241019.mlab.com:41019/goweek-backend',
+    {
+        useNewUrlParser: true
+    }
+);
 
 const app = express();
 
-app.get('/', (req, res) => {
-    return res.send('Hello World');
-});
+app.use(express.json());
+app.use(require('./routes'));
 
 app.listen(3000, () => {
     console.log('Servidor iniciado na porta 3000');
-})
+});
---
+++ backend/src/routes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const TweetController = require('./controllers/TweetController');
+const LikeController = require('./controllers/LikeController');
+
+const routes = express.Router();
+
+routes.get('/tweets', TweetController.index);
+routes.post('/tweets', TweetController.store);
+
+module.exports = routes;
