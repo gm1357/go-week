25959f398513c3683015716071fa95e8e9fcdd4d
Gabriel Machado <g_machado1@hotmail.com>
2018-11-06 14:34:00 -0200

    Adicionado socket.io e implementado emissão de eventos
    Adicionado também cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 mongoose.connect('mongodb://goweek:weekgo123@ds241019.mlab.com:41019/goweek-backend',
     {
@@ -9,9 +10,19 @@ mongoose.connect('mongodb://goweek:weekgo123@ds241019.mlab.com:41019/goweek-back
 
 const app = express();
 
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+app.use((req, res, next) => {
+    req.io = io;
+    
+    return next();
+});
+
+app.use(cors());
 app.use(express.json());
 app.use(require('./routes'));
 
-app.listen(3000, () => {
+server.listen(3000, () => {
     console.log('Servidor iniciado na porta 3000');
 });
